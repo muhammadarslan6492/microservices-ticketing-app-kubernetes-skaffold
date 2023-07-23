08805cff35d4b31e1a83a9f0b8f4a5799613b3ff
muhammad arslan <muhammadarslan6492@gmail.com>
2023-07-23 11:46:07 +0300

    .

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,10 @@
-import mongoose from 'mongoose';
+import exepress from 'express';
+import { json } from 'body-parser';
 
-import { app } from './app';
+const app = exepress();
 
-const start = async () => {
-  console.log('Starting up........');
+app.use(json());
 
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
-
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.error(err);
-  }
-
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
-  });
-};
-
-start();
+app.listen(3000, () => {
+  console.log('app running on 3000');
+});
