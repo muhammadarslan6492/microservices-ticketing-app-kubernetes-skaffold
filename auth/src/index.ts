da36901ce6f954d68bc3487d6f4a7daf5e303ffe
muhammad arslan <muhammadarslan6492@gmail.com>
2023-07-24 12:57:04 +0300

    .

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = exepress();
 
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi There');
+});
+
 app.listen(3000, () => {
   console.log('app running on 3000');
 });
