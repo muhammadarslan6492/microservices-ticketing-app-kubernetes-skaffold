5cd7050e89244721c92ab3e9dc2d2306e4db37bb
muhammad arslan <muhammadarslan6492@gmail.com>
2023-07-31 14:34:51 +0300

    .

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,8 +3,12 @@ import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+// router.get('/api/users/currentuser', currentUser, (req, res) => {
+//   res.send({ currentUser: req.currentUser || null });
+// });
+
+router.get('/api/users/currentuser', (req, res) => {
+  res.send({ message: 'muhammad arsalan is the current user' });
 });
 
 export { router as currentUserRouter };
